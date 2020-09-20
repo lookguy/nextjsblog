5827c90bf75414af0af2368a82ea80fbd6bb8727
招雄彬 <614619114@qq.com>
2020-09-20 18:12:05 +0800

    创建好一个新的首页CRM

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<React.Fragment>
+    <Head>
+      <title>next.js博客系统</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </React.Fragment>)
 }
 
 export default MyApp
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,17 @@
 import Head from 'next/head'
+import Layout from "../../components/layout"
+import Post from '../../styles/post.module.scss'
 
 export default function firstPost() {
   return (
-    <div>
+    <Layout>
       <Head>
         <title>不可思议的FIRST-POST页面</title>
       </Head>
-      <h2>不可思议的FIRST-POST</h2>
-      <p>
+      <h2 className={Post['title-h2']}>目前流浪在广州！</h2>
+      <p className={Post['content-p']}>
         李白乘舟将欲行，忽闻岸上踏歌声。
       </p>
-    </div>
+    </Layout>
   )
 }
